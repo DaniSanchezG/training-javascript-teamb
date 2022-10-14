3481b7a640b77128a7b5efb9a6f95d150489d190
Ana <analorenzo.2@hotmail.com>
2022-10-14 18:45:51 +0200

    Algorithms2.Ejercicio3

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+//ALGORITHMS 2. EJERCICIO 1
+
+let letter= [..."palindrome"];
+console.log(letter);
+console.log(`Palimdrome has ${letter.length} letters.`);
+
+function sayHello(moment, name) {
+  console.log(`Hi, good ${moment}, ${name}!`);
+  return name.length;
+}
