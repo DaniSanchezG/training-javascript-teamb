24e737872a360ae9a893c87a27b9725d76192531
Ana <analorenzo.2@hotmail.com>
2022-10-14 20:33:47 +0200

    Ejercicio 3+Ejercicio1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,35 @@
 //ALGORITHMS 2. EJERCICIO 1
 
-let letter= [..."palindrome"];
-console.log(letter);
-console.log(`Palimdrome has ${letter.length} letters.`);
+// let letter= [..."palindrome"];
+// console.log(letter);
+// console.log(`Palimdrome has ${letter.length} letters.`);
 
-function sayHello(moment, name) {
-  console.log(`Hi, good ${moment}, ${name}!`);
-  return name.length;
+// ALGORITHMS 3. EJERCICIO 1
+let min = parseInt(prompt("Please, insert the minimum value:"));
+let max = parseInt(prompt("Please, insert the maximun value:"));
+console.log(min);
+console.log(max);
+let valor = [];
+let auxMax=0;
+let auxMin = max;
+
+function numAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+for (let i = 0; i < 20; i++) {
+  valor[i] = numAleatorio(min, max);
+}
+for (let j = 0; j < valor.length; j++) {
+  if (valor[j] > auxMax) {
+    auxMax = valor[j];
+  }
+  if (valor[j] < auxMin) {
+    auxMin = valor[j];
+  }
+}
+console.log(valor);
+console.log(auxMax);
+console.log(auxMin);
+
+//ALGORITHMS 3. EJERCICIO4
