6cc08ad3f999b22b88aa992b38ce34f5f03cd6b2
nacho <nachomg93@gmail.com>
2022-10-15 14:41:50 +0200

    algorithms2-7

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,23 +14,31 @@
 // console.log(copyPalimdromo2);
 
 //ALGORITHMS2 EJERCICIO 7.
-// Mediante el objeto Math, genera un número aleatorio entero entre 0 y 10, y guárdalo en una variable.
-// A continuación pide al usuario que adivine el número. Tendrá como máximo 3 intentos. El
-// programa debe mostrar el número de intentos disponibles. Si acierta, mostramos el mensaje: “You
-// win!”. Si falla, pero le quedan intentos, mostramos el mensaje “Try it again”. Si agota los intentos y
-// no ha conseguido acertar, mostramos el mensaje: “Sorry, Good luck in love”.
 
 // let min = 0;
 // let max = 10;
-// let number = 9;
 // let numberRandom = Math.floor(Math.random() * (max - min + 1)) + min;
-// let guessNumber = prompt("Guess the number please");
-// let oneAttempts = prompt("Attempts one");
-// let twoAttempts = prompt("Attempts two");
-// let threeAttempts = prompt("Attempts tree");
+// console.log(numberRandom);
+// let guessNumber;
+// let centinela = false;
+// let intentos = 3;
 
+// while (!centinela && intentos >= 1) {
+//   if (intentos == 3) {
+//     guessNumber = parseInt(prompt(`Inserte un numero, intentos ${intentos}`));
+//   } else {
+//     guessNumber = parseInt(
+//       prompt(`Prueba de nuevo, tienes ${intentos} intentos`)
+//     );
+//   }
 
-// for (let i = 0; i < 5; i++) {
-//     console.count();
-
-
+//   if (numberRandom == guessNumber) {
+//     centinela = true;
+//   }
+//   intentos--;
+// }
+// if (centinela) {
+//   console.log("You wins!");
+// } else {
+//   console.log("Sorry, Good luck in love!");
+// }
