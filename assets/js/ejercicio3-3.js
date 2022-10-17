e3515ce378a6ec5203c115c24fb4362ffbe0196a
Ismael <ismaelboulen@gmail.com>
2022-10-17 16:06:09 +0200

    Revisar ejercicio

---
+++ assets/js/ejercicio3-3.js
@@ -0,0 +1,11 @@
+//ejercicio 3.3
+let matrx = new Array(0);
+matrx.push([]);
+matrx[0] = [`*`,"*","*","*"];
+matrx.push([]);
+matrx[1] = ["*","*","*","*"];
+matrx.push([]);
+matrx[2] = ["*","*","*","*"];
+matrx.push([]);
+matrx[3] = ["*","*","*","*"];
+console.log(matrx);
