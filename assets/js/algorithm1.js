0e76ad5a6309c98dddbad4d3b11a21bffb54a399
DaniSanchezG <laughingoutloud90@gmail.com>
2022-10-31 13:48:15 +0100

    Algorithm 3 Exercise 9 Finished

--- assets/js/algorithm1.js
+++ assets/js/algorithm1.js
@@ -430,113 +430,180 @@
 
 // Algorithm 3 Exercise 8
 
-let ramdonCard = (max, min) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+// let ramdonCard = (max, min) => {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// };
 
-let playerBet = () => {
-  bet = parseInt(prompt("How much do you want to bet?"));
-  while(isNaN(bet)){
-    bet = parseInt(prompt("opss! wrong option, try againt, How much do you want to bet?"));
-  }
-  if (bet > player.money) {
-    alert("You don't have enough money");
-    playerBet();
-  } else {
-    return bet;
-  }
-};
+// let playerBet = () => {
+//   bet = parseInt(prompt("How much do you want to bet?"));
+//   while(isNaN(bet)){
+//     bet = parseInt(prompt("opss! wrong option, try againt, How much do you want to bet?"));
+//   }
+//   if (bet > player.money) {
+//     alert("You don't have enough money");
+//     playerBet();
+//   } else {
+//     return bet;
+//   }
+// };
 
-let checkOption = (optionString) => {
-  while (optionString !== "y" && optionString !== "n") {
-    optionString = prompt("Please, enter a valid option (y/n)");
-  }
-  if (optionString === "y") {
-    return optionString;
-  } else {
-    return optionString;
-  }
-};
+// let checkOption = (optionString) => {
+//   while (optionString !== "y" && optionString !== "n") {
+//     optionString = prompt("Please, enter a valid option (y/n)");
+//   }
+//   if (optionString === "y") {
+//     return optionString;
+//   } else {
+//     return optionString;
+//   }
+// };
 
-let suitOfCards = ["Hearts", "Diamonds", "Clubs", "Spades"];
-let cardNumbers = [
-  "ACE",
-  "KING",
-  "QUEEN",
-  "JACK",
-  "10",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
-  "2",
-];
-let optionString = "";
+// let suitOfCards = ["Hearts", "Diamonds", "Clubs", "Spades"];
+// let cardNumbers = [
+//   "ACE",
+//   "KING",
+//   "QUEEN",
+//   "JACK",
+//   "10",
+//   "9",
+//   "8",
+//   "7",
+//   "6",
+//   "5",
+//   "4",
+//   "3",
+//   "2",
+// ];
+// let optionString = "";
 
-let player = {
-  cardNumber: 0,
-  cardSuit: "",
-  money: 500,
-  randomNumber: -1,
-};
+// let player = {
+//   cardNumber: 0,
+//   cardSuit: "",
+//   money: 500,
+//   randomNumber: -1,
+// };
 
-let dealer = {
-  cardNumber: 0,
-  cardSuit: "",
-  randomNumber: -1,
-};
+// let dealer = {
+//   cardNumber: 0,
+//   cardSuit: "",
+//   randomNumber: -1,
+// };
 
-do {
-  if (player.money <= 0) {
-    alert("You don't have enough money to play");
-  } else {
-    playerBet();
-    player.randomNumber = ramdonCard(13, 0);
-    player.cardNumber = cardNumbers[player.randomNumber];
-    player.cardSuit = suitOfCards[ramdonCard(4, 0)];
-    dealer.randomNumber = ramdonCard(13, 0);
-    dealer.cardNumber = cardNumbers[dealer.randomNumber];
-    dealer.cardSuit = suitOfCards[ramdonCard(4, 0)];
-    while (
-      player.cardNumber === dealer.cardNumber &&
-      player.cardSuit === dealer.cardSuit
-    ) {
-      dealer.randomNumber = ramdonCard(13, 0);
-      dealer.cardNumber = cardNumbers[dealer.randomNumber];
-      dealer.cardSuit = suitOfCards[ramdonCard(4, 0)];
-    }
-    console.log(`Your card is ${player.cardNumber} of ${player.cardSuit}`);
-    console.log(`Dealer card is ${dealer.cardNumber} of ${dealer.cardSuit}`);
-    if (player.randomNumber < dealer.randomNumber) {
-      alert("You win");
-      player.money += bet;
-    } else if (player.randomNumber > dealer.randomNumber) {
-      alert("You lose");
-      player.money -= bet;
-    } else {
-      alert("Draw");
-    }
-    console.log(`You have ${player.money} €`);
-    optionString = prompt("Do you want to play again? (y/n)");
-    checkOption(optionString);
-  }
-} while (player.money > 0 && optionString === "y");
+// do {
+//   if (player.money <= 0) {
+//     alert("You don't have enough money to play");
+//   } else {
+//     playerBet();
+//     player.randomNumber = ramdonCard(13, 0);
+//     player.cardNumber = cardNumbers[player.randomNumber];
+//     player.cardSuit = suitOfCards[ramdonCard(4, 0)];
+//     dealer.randomNumber = ramdonCard(13, 0);
+//     dealer.cardNumber = cardNumbers[dealer.randomNumber];
+//     dealer.cardSuit = suitOfCards[ramdonCard(4, 0)];
+//     while (
+//       player.cardNumber === dealer.cardNumber &&
+//       player.cardSuit === dealer.cardSuit
+//     ) {
+//       dealer.randomNumber = ramdonCard(13, 0);
+//       dealer.cardNumber = cardNumbers[dealer.randomNumber];
+//       dealer.cardSuit = suitOfCards[ramdonCard(4, 0)];
+//     }
+//     console.log(`Your card is ${player.cardNumber} of ${player.cardSuit}`);
+//     console.log(`Dealer card is ${dealer.cardNumber} of ${dealer.cardSuit}`);
+//     if (player.randomNumber < dealer.randomNumber) {
+//       alert("You win");
+//       player.money += bet;
+//     } else if (player.randomNumber > dealer.randomNumber) {
+//       alert("You lose");
+//       player.money -= bet;
+//     } else {
+//       alert("Draw");
+//     }
+//     console.log(`You have ${player.money} €`);
+//     optionString = prompt("Do you want to play again? (y/n)");
+//     checkOption(optionString);
+//   }
+// } while (player.money > 0 && optionString === "y");
 
-if (player.money <= 0) {
-  console.log("You don't have enough money to play");
-  console.log(`You lost ${500 - player.money} €`);
-  console.log(`You have ${player.money} €`);
-} else {
-  console.log("Thanks for playing");
-  if (player.money > 500) {
-    console.log(`You won ${player.money - 500} €`);
-    console.log(`You have ${player.money} €`);
-  } else {
-    console.log(`You lost ${500 - player.money} €`);
-    console.log(`You have ${player.money} €`);
-  }
-}
+// if (player.money <= 0) {
+//   console.log("You don't have enough money to play");
+//   console.log(`You lost ${500 - player.money} €`);
+//   console.log(`You have ${player.money} €`);
+// } else {
+//   console.log("Thanks for playing");
+//   if (player.money > 500) {
+//     console.log(`You won ${player.money - 500} €`);
+//     console.log(`You have ${player.money} €`);
+//   } else {
+//     console.log(`You lost ${500 - player.money} €`);
+//     console.log(`You have ${player.money} €`);
+//   }
+// }
+
+// Algorithm 3 Exercise 9
+
+// let checkOption = (optionString) => {
+//   while (optionString !== "c" && optionString !== "d") {
+//     optionString = prompt("Please, enter a valid option (c/d)");
+//   }
+//   if (optionString === "c") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// let codify = (message) => {
+//   let result = "";
+//   for (let i = 0; i < message.length; i++) {
+//   if(message[i].charCodeAt() === 88){
+//     result += String.fromCharCode(message.charCodeAt(65) + 3);
+//   }else if(message[i].charCodeAt() === 89){
+//     result += String.fromCharCode(message.charCodeAt(66) + 3);
+//   }else if(message[i].charCodeAt() === 90){
+//     result += String.fromCharCode(message.charCodeAt(67) + 3);
+//   }else{
+//     result += String.fromCharCode(message.charCodeAt(i) + 3);
+//   }
+// }
+//   return result;
+// };
+
+// let decodify = (message) => {
+//   let result = "";
+//   for (let i = 0; i < message.length; i++) {
+//   if(message[i].charCodeAt() === 65){
+//     result += String.fromCharCode(message.charCodeAt(88) - 3);
+//   }else if(message[i].charCodeAt() === 66){
+//     result += String.fromCharCode(message.charCodeAt(89) - 3);
+//   }else if(message[i].charCodeAt() === 67){
+//     result += String.fromCharCode(message.charCodeAt(90) - 3);
+//   }else{
+//     result += String.fromCharCode(message.charCodeAt(i) - 3);
+//   }
+// }
+//   return result;
+// }
+
+// let codeAndDecode = (message, cOrD) => {
+//   if (cOrD === true) {
+//     return codify(message);
+//   } else {
+//     return decodify(message);
+//   }
+// }
+
+// let message = prompt("Enter a message").toUpperCase();
+// let chooseCodifyOrDecodify = prompt("Do you want to codify or decodify? (c/d)");
+// let cOrD = checkOption(chooseCodifyOrDecodify);
+// console.log(codeAndDecode(message, cOrD));
+
+
+
+
+
+
+
+
+
 
